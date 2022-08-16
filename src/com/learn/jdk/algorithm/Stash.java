0362ae733c85d4d095ef78ce1f5e517c02585208
“yuezp” <yuezpchn@126.com>
2022-08-16 19:45:17 +0800

    test stash Stash

---
+++ src/com/learn/jdk/algorithm/Stash.java
@@ -0,0 +1,12 @@
+package com.learn.jdk.algorithm;
+
+/**
+ * @Author yuezp
+ * @Date 2022/8/16 下午7:35
+ * @Version v1.0
+ */
+public class Stash {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
