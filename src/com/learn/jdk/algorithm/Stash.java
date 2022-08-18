99278c8321fee001dc66decbd3b4146d8032e8da
“yuezp” <yuezpchn@126.com>
2022-08-18 19:14:03 +0800

    555555

--- src/com/learn/jdk/algorithm/Stash.java
+++ src/com/learn/jdk/algorithm/Stash.java
@@ -10,6 +10,7 @@ public class Stash {
     public static void main(String[] args) {
         System.out.println("1111111");
         System.out.println("2222222");
+        System.out.println("5555555");
 
     }
 }
