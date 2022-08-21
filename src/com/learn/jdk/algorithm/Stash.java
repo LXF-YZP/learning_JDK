e8f6c59ee13a0a27c5ce92c80329b607ac26d060
“yuezp” <yuezpchn@126.com>
2022-08-22 07:39:22 +0800

    master 4

--- src/com/learn/jdk/algorithm/Stash.java
+++ src/com/learn/jdk/algorithm/Stash.java
@@ -12,6 +12,7 @@ public class Stash {
         System.out.println("master 1");
         System.out.println("master 2");
         System.out.println("master 3");
+        System.out.println("master 4");
 
     }
 }
