b365ae5005cf2efa265e90c76a1b0d073844a9e5
“yuezp” <yuezpchn@126.com>
2022-08-22 06:35:17 +0800

    master分支基础

--- src/com/learn/jdk/algorithm/Stash.java
+++ src/com/learn/jdk/algorithm/Stash.java
@@ -9,8 +9,6 @@ public class Stash {
 
     public static void main(String[] args) {
         System.out.println("分支基础");
-        System.out.println("master第一次提交");
-        System.out.println("master第二次提交");
 
     }
 }
